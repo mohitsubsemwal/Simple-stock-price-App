4b3c0151677a894ba998414a9a91e25a09366459
Mohit Semwal <83613956+mohitsubsemwal@users.noreply.github.com>
2021-06-10 04:48:06 -0300

    Add files via upload

---
+++ StockPriceApp.py
@@ -0,0 +1,20 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Simple stock price App
+
+Shown are stock closing price and volume of Google!
+
+""")
+
+tickerSymbol = 'GOOGL'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2021-5-31')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
